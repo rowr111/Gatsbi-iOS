f305756e18399cb9508869f35461561538f0c144
Jeanie <jeanie.conner@gmail.com>
2015-06-07 22:53:35 -0700

    Initial Commit

---
+++ Gatsbi_iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gatsbi_iOS
+//
+//  Created by Jeanie Conner on 6/7/15.
+//  Copyright (c) 2015 Gatsbi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
