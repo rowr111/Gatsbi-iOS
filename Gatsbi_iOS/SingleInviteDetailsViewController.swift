cae18ec9ed87430a5d59d3c1f45e4e7605c4cfb1
Jeanie <jeanie.conner@gmail.com>
2016-01-21 01:13:57 -0800

    updates to single invite view

--- Gatsbi_iOS/SingleInviteDetailsViewController.swift
+++ Gatsbi_iOS/SingleInviteDetailsViewController.swift
@@ -29,9 +29,13 @@ class SingleInviteDetailsViewController: UIViewController {
     @IBOutlet weak var inviteMessage: UILabel!
     
     @IBOutlet weak var inviteTitleLabel: UILabel!
-
-    @IBAction func inviteRSVPStatus(sender: UIButton) {
-    }
+    
+    @IBOutlet weak var goingLabel: UILabel!
+    @IBOutlet weak var goingList: UILabel!
+    @IBOutlet weak var notRespondedLabel: UILabel!
+    @IBOutlet weak var notRespondedList: UILabel!
+    @IBOutlet weak var notGoingLabel: UILabel!
+    @IBOutlet weak var notGoingList: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -49,9 +53,10 @@ class SingleInviteDetailsViewController: UIViewController {
         formatter.dateStyle = NSDateFormatterStyle.LongStyle
         formatter.timeStyle = .ShortStyle
         
-
         self.inviteDateRange.text = "\(formatter.stringFromDate(myInvite.Date)) to \r\n\(formatter.stringFromDate(myInvite.EndDate))"
         self.inviteDateRange.sizeToFit()
+        
+        loadAttendeeList()
         
     }
     
@@ -74,5 +79,68 @@ class SingleInviteDetailsViewController: UIViewController {
         locationIcon.image = IonIcons.imageWithIcon(ion_ios_location, iconColor: UIColor.whiteColor(), iconSize: 27, imageSize: CGSizeMake(30, 30))
         messageIcon.image = IonIcons.imageWithIcon(ion_ios_paper, iconColor: UIColor.whiteColor(), iconSize: 27, imageSize: CGSizeMake(30, 30))
         guestsIcon.image = IonIcons.imageWithIcon(ion_ios_people, iconColor: UIColor.whiteColor(), iconSize: 27, imageSize: CGSizeMake(30, 30))
+    }
+    
+    func loadAttendeeList()
+    {
+        var goingCount:Int = 0
+        var notGoingCount:Int = 0
+        var notRespondedCount:Int = 0
+        var goingList:String = ""
+        var notGoingList:String = ""
+        var notRespondedList:String = ""
+        var emailaddr:String = ""
+        
+        let query = PFQuery(className:"UserInviteEvent")
+        query.whereKey("InviteObjectID", equalTo: self.myInviteEvent!.InviteObjectID)
+        if let objects = query.findObjects() {
+            print("found \(objects.count) number of invites")
+            for inviteEvent in objects {
+                if let myemailaddr=inviteEvent["EmailAddr"] as? String
+                {
+                    print(myemailaddr)
+                    emailaddr = myemailaddr
+                }
+                else
+                { emailaddr = "Mystery Guest" }
+                if let host=inviteEvent["Host"] as? Bool
+                {
+                    if host {emailaddr = "\(emailaddr) (Host)"}
+                }
+                if let attending = inviteEvent["Attending"] as? Bool
+                {
+                    if (attending)
+                    {
+                        goingCount = goingCount + 1
+                        if goingCount == 1
+                        { goingList = emailaddr }
+                        else
+                        { goingList = "\(goingList)\r\n\(emailaddr)" }
+                    }
+                    else
+                    {
+                        notGoingCount = notGoingCount + 1
+                        if notGoingCount == 1
+                        { notGoingList = emailaddr }
+                        else
+                        { notGoingList = "\(notGoingList)\r\n\(emailaddr)" }
+                    }
+                }
+                else
+                {
+                    notRespondedCount = notRespondedCount + 1
+                    if notRespondedCount == 1
+                    { notRespondedList = emailaddr}
+                    else
+                    { notRespondedList = "\(notRespondedList)\r\n\(emailaddr)"}
+                }
+                self.goingLabel.text = "Going (\(goingCount))"
+                self.goingList.text = goingList
+                self.notRespondedLabel.text = "Not Responded (\(notRespondedCount))"
+                self.notRespondedList.text = notRespondedList
+                self.notGoingLabel.text = "Not Going (\(notGoingCount))"
+                self.notGoingList.text = notGoingList
+            }
+        }
     }
 }
