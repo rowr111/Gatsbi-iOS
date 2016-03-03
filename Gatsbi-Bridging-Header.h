eb7d41b98f883d2b86c42c85f50185c370acf0ff
Jeanie <jeanie.conner@gmail.com>
2016-03-03 00:27:42 -0800

    adding new parse files

--- Gatsbi-Bridging-Header.h
+++ Gatsbi-Bridging-Header.h
@@ -13,7 +13,6 @@
 #import "Gatsbi_iOS/GKImageCropViewController.h"
 #import "Gatsbi_iOS/GKResizeableCropOverlayView.h"
 #import <Parse/Parse.h>
-#import <Parse/ParseClientConfiguration.h>
 #import <ParseUI/ParseUI.h>
 #import <FBSDKCoreKit/FBSDKCoreKit.h>
 #import <FBSDKLoginKit/FBSDKLoginKit.h>
